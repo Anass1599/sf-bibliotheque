f3973513e10e16ca25f037d788e870078427dea9
Anass1599 <anassmoujahid@hotmail.fr>
2021-12-06 16:45:58 +0100

    author

---
+++ sf-biblio/src/Controller/authorController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class authorController extends AbstractController
+{
+
+    /**
+     * @Route("/authors", name="authors")
+     */
+    public function authors(AuthorRepository $authorRepository)
+    {
+        // j'utilise la méthode findAll de la classe AuthorRepository
+        // pour récupérer tous les auteurs de la table author
+        $authors = $authorRepository->findAll();
+
+
+        // je veux utiliser un fichier HTML en tant que réponse
+        // HTTP
+        // pour ça j'appelle la méthode render (issue de l'AbstractController)
+        // et je lui passe en premier parametre le nom / le chemin du fichier
+        // twig (html) situé dans le dossier template
+        //et aussi ma variable authors.
+        return $this->render('authors.html.twig', ['authors' => $authors]);
+    }
+
+    /**
+     * @Route("/author/{id}", name="author")
+     */
+    public function author($id, AuthorRepository $authorRepository)
+    {
+        // j'utilise la méthode findAll de la classe AuthorRepository
+        // pour récupérer tous les auteurs de la table author
+        $author = $authorRepository->find($id);
+
+
+        // je veux utiliser un fichier HTML en tant que réponse
+        // HTTP
+        // pour ça j'appelle la méthode render (issue de l'AbstractController)
+        // et je lui passe en premier parametre le nom / le chemin du fichier
+        // twig (html) situé dans le dossier template
+        //et aussi ma variable author.
+        return $this->render('author.html.twig', ['author' => $author]);
+    }
+
+}
--- sf-biblio/src/Controller/homeController.php
+++ sf-biblio/src/Controller/homeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -16,65 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 class homeController extends AbstractController
 {
 
-    // je créé une propriété privé $books
-    // qui contiendra la liste de tous les livres
-    // afin d'éviter de répéter cette variable dans toutes les méthodes
-    private $books;
-
-    // je créé une méthode constructor, qui sera appelée automatiquement
-    // quand la classe sera instanciée
-    // Symfony instancie les classes de controller (même si on le voit pas)
-    // pour afficher les pages créées dans le controleur
-    public function __construct() {
-
-        // je définis la valeur de la propriété livres.
-        $this->books = [
-            1 => [
-                "title" => "Dune",
-                "author" => "Franck Herbert",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://images-na.ssl-images-amazon.com/images/I/41rDK8Jb1LL._SX312_BO1,204,203,200_.jpg",
-                "id" => 1
-            ],
-            2 => [
-                "title" => "Silo",
-                "author" => "Tery Hayes",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://images.archambault.ca/images/PG/1856/1856375-gf.jpg?404=default&w=400",
-                "id" => 2
-            ],
-            3 => [
-                "title" => "Win",
-                "author" => "Harlan Coben",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://m.media-amazon.com/images/I/51IgnZIwYRS.jpg",
-                "id" => 3
-            ],
-            4 => [
-                "title" => "La part de l'autre",
-                "author" => "Éric-Emmanuel Schmitt",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://images-na.ssl-images-amazon.com/images/I/71yoJZCdSaL.jpg",
-                "id" => 4
-            ],
-            5 => [
-                "title" => "Snowman",
-                "author" => "Jo Nesbo",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://images2.medimops.eu/product/ccc076/M01784700924-large.jpg",
-                "id" => 5
-            ],
-            6 => [
-                "title" => "Intuitio",
-                "author" => "Laurent Gounelle",
-                "publishedAt" => new \DateTime('NOW'),
-                "image" => "https://calmann-levy.fr/sites/default/files/images/livres/couv/9782702182932-001-T.jpeg",
-                "id" => 6
-            ]
-
-        ];
-
-    }
 
     /**
      * je créé une route (donc une page)
@@ -84,18 +26,19 @@ class homeController extends AbstractController
      * est placée au dessus de la méthode
      * @Route("/home", name="home")
      */
-    public function home(BookRepository $bookRepository)
+    public function home(BookRepository $bookRepository, AuthorRepository $authorRepository)
     {
 
         //je utilise la méthode findBy() pour extrait une portion de mon tableau dans un autre tableau.
-        $tabs = $bookRepository->findBy(array(),array('id'=>'DESC'),3,0);
+        $books = $bookRepository->findBy(array(),array('id'=>'DESC'),3,0);
+        $authors = $authorRepository->findBy(array(),array('id'=>'DESC'),3,0);
         // je veux utiliser un fichier HTML en tant que réponse
         // HTTP
         // pour ça j'appelle la méthode render (issue de l'AbstractController)
         // et je lui passe en premier parametre le nom / le chemin du fichier
         // twig (html) situé dans le dossier template
         //et aussi mon tableau tabs.
-        return $this->render("home.html.twig", ['tabs' => $tabs]);
+        return $this->render("home.html.twig", ['books' => $books,'authors'=>$authors]);
     }
 
     /**
